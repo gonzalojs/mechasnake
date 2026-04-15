e0d4c25747fb8ddf69c63972b2c397690610073f
gonzalojs <eogutier@gmail.com>
2026-04-15 16:59:33 -0400

    Fix : movimiento limitado a volver por donde venia.

--- scripts/button_capture.gd
+++ scripts/button_capture.gd
@@ -3,11 +3,24 @@ class_name ButtonCaptureComponent extends Node
 var move_dir : Vector2 = Vector2.RIGHT #Direccion inicial
 
 func _unhandled_input(event: InputEvent) -> void:
+	#No volver por el mismo camino para no chocar contra todo el cuerpo
 	if Input.is_action_pressed("up"):
-		move_dir = Vector2.UP
+		if move_dir == Vector2.DOWN:
+			move_dir = Vector2.DOWN
+		else:
+			move_dir = Vector2.UP
 	if Input.is_action_pressed("down"):
-		move_dir = Vector2.DOWN
+		if move_dir == Vector2.UP:
+			move_dir = Vector2.UP
+		else:
+			move_dir = Vector2.DOWN
 	if Input.is_action_pressed("left"):
-		move_dir = Vector2.LEFT
+		if move_dir == Vector2.RIGHT:
+			move_dir = Vector2.RIGHT
+		else:
+			move_dir = Vector2.LEFT
 	if Input.is_action_pressed("right"):
-		move_dir = Vector2.RIGHT
+		if move_dir == Vector2.LEFT:
+			move_dir = Vector2.LEFT
+		else:
+			move_dir = Vector2.RIGHT
