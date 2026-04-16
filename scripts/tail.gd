a7f7a9bf73eb04e4afda6a72a9b058ab03aa441f
gonzalojs <eogutier@gmail.com>
2026-04-16 17:39:32 -0400

    rm snake tail collision with fruit. Creaba dos frutas. Changed collisions of partes

--- scripts/tail.gd
+++ scripts/tail.gd
@@ -5,12 +5,5 @@ class_name Tail extends SnakePart
 
 @onready var sprite: Sprite2D = $Sprite
 
-
 func _ready() -> void:
 	sprite.texture = textures.pick_random()
-
-
-func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("food"):
-		Signals.food_eaten.emit()
-		area.call_deferred("queue_free")
