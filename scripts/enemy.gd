45291bd81350b2adc73424119d41f89b24d2bcfc
gonzalojs <eogutier@gmail.com>
2026-04-21 21:10:14 -0400

    Enemy following snake

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,3 +1,10 @@
 extends CharacterBody2D
 
 @export var movement_speed : float = 20.0
+
+@onready var snake = get_tree().get_first_node_in_group("snake")
+
+func _physics_process(_delta: float) -> void:
+	var direction = global_position.direction_to(snake.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
