f5e9b8f28b996bb6652f53ba77820d04e85f8067
gonzalojs <eogutier@gmail.com>
2026-04-21 21:49:41 -0400

    Enemy following snake

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,18 @@ extends CharacterBody2D
 
 @onready var snake = get_tree().get_first_node_in_group("snake")
 
+var snakes : Array = []
+
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(snake.global_position)
 	velocity = direction * movement_speed
 	move_and_slide()
+
+
+func _on_detection_area_area_entered(area: Area2D) -> void:
+	snakes.append(area.name)
+	print(snakes)
+
+
+func _on_detection_area_area_exited(area: Area2D) -> void:
+	snakes.erase(area.name)
